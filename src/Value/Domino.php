c8f39e5883c01f0c3cf283a6e75361ddcf9a66bb
Alex Patterson <alex.patterson.webdev@gmail.com>
2020-07-11 17:08:41 +0000

    - New test case coverage of the player implementation - Created new domino value object with various methods to return the value of the tiles/dots

---
+++ src/Value/Domino.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arp\DominoGame\Value;
+
+/**
+ * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
+ * @package Arp\DominoGame\Value
+ */
+class Domino
+{
+    /**
+     * @var int
+     */
+    private int $topTile;
+
+    /**
+     * @var int
+     */
+    private int $bottomTile;
+
+    /**
+     * @param int $topTile
+     * @param int $bottomTile
+     */
+    public function __construct(int $topTile, int $bottomTile)
+    {
+        $this->topTile = $topTile;
+        $this->bottomTile = $bottomTile;
+    }
+
+    /**
+     * Check if the top and bottom dots are equal, a double.
+     *
+     * @return bool
+     */
+    public function isDouble(): bool
+    {
+        return $this->topTile === $this->bottomTile;
+    }
+
+    /**
+     * Return the number of dots in the 'top' square.
+     *
+     * @return int
+     */
+    public function getTopTile(): int
+    {
+        return $this->topTile;
+    }
+
+    /**
+     * Return the number of dots in the 'bottom' square.
+     *
+     * @return int
+     */
+    public function getBottomTitle(): int
+    {
+        return $this->bottomTile;
+    }
+
+    /**
+     * Return the sum of the domino's dots.
+     *
+     * @return int
+     */
+    public function getValue(): int
+    {
+        return ($this->topTile + $this->bottomTile);
+    }
+
+    /**
+     * Check if both provided $topTitle and $bottomTile values match the current domino values.
+     *
+     * @param int $topTitle
+     * @param int $bottomTile
+     *
+     * @return bool
+     */
+    public function isTileMatch(int $topTitle, int $bottomTile): bool
+    {
+        return $this->isTopTileMatch($topTitle) && $this->isBottomTitleMatch($bottomTile);
+    }
+
+    /**
+     * @param int $topTitle
+     *
+     * @return bool
+     */
+    public function isTopTileMatch(int $topTitle): bool
+    {
+        return $this->topTile === $topTitle;
+    }
+
+    /**
+     * @param int $bottomTile
+     *
+     * @return bool
+     */
+    public function isBottomTitleMatch(int $bottomTile): bool
+    {
+        return $this->bottomTile === $bottomTile;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->topTile . ' | ' . $this->bottomTile;
+    }
+}
---
+++ test/phpunit/Value/PlayerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArpTest\Value\PlayerTest;
+
+use Arp\DominoGame\Value\Player;
+use Arp\DominoGame\Value\PlayerInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
+ * @package ArpTest\Value\PlayerTest
+ */
+final class PlayerTest extends TestCase
+{
+    /**
+     * Assert that the player implements PlayerInterface
+     *
+     * @covers \Arp\DominoGame\Value\Player::__construct
+     */
+    public function testImplementsPlayerInterface(): void
+    {
+        $player = new Player();
+
+        $this->assertInstanceOf(PlayerInterface::class, $player);
+    }
+
+    /**
+     * Assert that the player starts with an empty hand.
+     *
+     * @covers \Arp\DominoGame\Value\Player::getHand
+     */
+    public function testPlayerStartsWithAnEmptyHand(): void
+    {
+        $player = new Player();
+
+        $this->assertSame(0, $player->getHand()->count());
+    }
+}
